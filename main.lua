85b5a8a00f726f1ce92cadd321e0bfe8405efcf5
DMatthieu <matt.delporte6@gmail.com>
2019-09-06 22:49:00 +0200

    Git Init: Base project

---
+++ main.lua
@@ -0,0 +1,37 @@
+--Cette ligne permet d'afficher des traces dans la console pendant l'éxécution
+io.stdout:setvbuf('no');
+
+--love.graphics.setDefaultFilter( "nearest" )
+
+function love.load()
+    --DIMENSIONS DE LA FENETRE
+    windowWidth = love.graphics.getWidth();
+    windowHeight = love.graphics.getHeight();
+
+    
+end
+
+function love.update(dt)
+
+    
+
+end
+
+function love.draw()
+    
+end
+
+--Callback checking if mouse is clicked
+function love.mousepressed(x, y, button, istouch, presses)
+    
+end
+
+
+
+--Permit Game Extinction
+function love.keypressed(key)
+    if key=="escape" then
+        love.event.quit()
+    end  
+end
+
