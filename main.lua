6ebbe66ce907de653e0dd3bb8b05801dc12dcae7
Jakeblues32 <55003507+Jakeblues32@users.noreply.github.com>
2019-09-23 22:56:02 +0200

    mise à jour déplacements
    J'ai fais une amélioration pour les déplacements qui ont désormais un effet basé sur le delta time ce me permet de maintenir le déplacement si le joueur saute ou tombe. Il me reste à implémenter à ça le perso qui peut grimper sur les tiles murs ou corniches (avec un effet variant selon l'un ou l'autre) et là je serais à peu près au point avec ma gestion de physique. Teste déja cette version et tu me donnera tes impressions voilà tout ^^

--- main.lua
+++ main.lua
@@ -35,8 +35,8 @@ function love.load()
   tilemap1[5]  = {0,0,1,0,0,0,0,0,0,0,0,0,0,0}
   tilemap1[6]  = {1,1,1,1,1,1,0,0,0,0,0,0,0,0}
   tilemap1[7]  = {0,0,0,0,1,0,0,0,0,2,0,2,0,0}
-  tilemap1[8]  = {0,0,0,0,0,1,1,1,1,1,1,1,1,0}
-  tilemap1[9]  = {0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+  tilemap1[8]  = {0,0,0,0,0,1,1,1,1,0,0,0,0,0}
+  tilemap1[9]  = {0,0,0,0,0,0,0,0,0,1,1,1,0,0}
   tilemap1[10] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1}
 
 
@@ -58,8 +58,9 @@ function love.load()
     player.h = 32
     player.vx = 0
     player.vy = 0
-    --player.friction = 100
-    player.speed = 3
+    player.friction = 300
+    player.accel = 100
+    player.maxspeed = 3
     player.gravity = 20
     player.jump = 400
     player.state = "falling"
@@ -76,22 +77,21 @@ end
 
 
 
+
 function love.update(dt)
   
   
   function love.keypressed(key)
     
-    if key == "space" then
+    if key == "space" then  -- pour le saut
       
-      if player.state == "jumping" or player.state == "falling" then
+      if player.state ~= "jumping" and player.state ~= "falling" then -- si personnage n'est pas déja en l'air
         
-      else
-        
-        player.vy = player.vy - player.jump*dt  --application d'une force de saut fixe
+        player.vy = player.vy - player.jump*dt  -- alors application d'une force de saut donnée
         
         player.state = "jumping"
         
-        timerjumping = player.jump
+        -- timerjumping = player.jump  --  voir plus tard
         
       end
       
@@ -100,17 +100,38 @@ function love.update(dt)
   end
 
   
-  -- début de la gestion de la physique joueur
+  -- début de la gestion de la physique du joueur
   
     -- physique horizontale
+  
+  move(dt, player)    -- fonction de déplacement avec friction en cas d'arrêt hors saut
+  
+  checkcollisionfenetre()  -- premier check de collision du bord de la fenêtre
+  
+  collisionmurs(player)   -- premier check de collision avec les murs
 
-  for a = 1, player.speed, 1 do       -- pour chaque pixel de déplacement en fonction de la vitesse du joueur
+  for a = 1, math.abs(player.vx), 1 do       -- pour chaque pixel de déplacement en fonction de la vitesse du joueur
     
     checkcollisionfenetre()           -- je vérifie la collision avec le bord de la fenêtre
     
     collisionmurs(player)             -- je vérifie la collision avec les murs formés par ma hitboxmap
     
+    if player.vx > 0 then                                       -- en fonction de mon vx je déplace le joueur : 
+        
+        player.x = player.x + 1                                         -- vers la droite
+        
+      elseif player.vx < 0 then
+        
+        player.x = player.x - 1                                         -- vers la gauche
+        
+      elseif player.vx == 0 then
+        
+        break                                                           -- si pas de déplacement à faire (collision) : fin prématurée de la boucle de déplacement 
+        
+      end
+    
   end
+  
   
     -- physique verticale
   
@@ -119,7 +140,6 @@ function love.update(dt)
     player.vy = player.vy + player.gravity*dt  -- si le joueur saute ou tombe on lui applique une gravité
     
   end
-  
   
   
   collisionhautbas(player)   -- premier test de détermination de l'état du joueur
@@ -165,7 +185,7 @@ end
 function love.draw()
   
   
-  for a = 1, 10, 1 do
+  for a = 1, 10, 1 do      -- dessin des tiles
     for b = 1, 14, 1 do
       if tilemap1[a][b] == 1 then
         love.graphics.draw(img1, tile1, (b-1)*32, (a-1)*32)
@@ -184,7 +204,11 @@ end
 
 
 
+
+
 -------------------------------------------------------------------------------------------------
+-- LISTE DES FONCTIONS UTILISEES
+
 
   function hitbox(tilemap)
     
@@ -192,7 +216,7 @@ end
       
       for b = 1, 14, 1 do
         
-        if tilemap[a][b] == 1 then
+        if tilemap[a][b] == 1 then       -- A AMELIORER !!!!!!!!!!!!!!!!! pour intégrer tous les types de tiles
           
           hitboxmap[#hitboxmap + 1] = {}
             
@@ -201,6 +225,7 @@ end
             hitboxmap[#hitboxmap].w = tilesize
             hitboxmap[#hitboxmap].h = tilesize
             hitboxmap[#hitboxmap].coll = false
+            -- hitboxmap[#hitboxmap].state = tilemap[a][b]  -- compris dans la future amélioration ne pas en tenir compte pour le moment
           
         end
         
@@ -245,6 +270,61 @@ end
   
   
   
+  function move(dt, player) 
+    
+    if player.state == "normal" then
+        
+      if love.keyboard.isDown("q") then
+        
+        player.vx = player.vx - player.accel*dt
+        
+        if math.abs(player.vx) >= player.maxspeed then
+          
+          player.vx = - player.maxspeed
+          
+        end
+        
+      elseif love.keyboard.isDown("d") then
+        
+        player.vx = player.vx + player.accel*dt
+        
+        if math.abs(player.vx) >= player.maxspeed then
+          
+          player.vx = player.maxspeed
+          
+        end
+        
+      else
+        
+        if player.vx < 0 then
+          
+          player.vx = player.vx + player.friction*dt
+          
+          if player.vx > 0 then
+            
+            player.vx = 0
+            
+          end
+          
+        elseif player.vx > 0 then
+          
+          player.vx = player.vx - player.friction * dt
+          
+          if player.vx < 0 then
+            
+            player.vx = 0
+            
+          end
+          
+        end
+        
+      end
+      
+    end
+    
+  end
+  
+  
   
   function resetcollisions()
     
@@ -281,7 +361,11 @@ end
             end
             
             if collisiongauche == false then
-              player.x = player.x - 1  
+              
+            else             ------------------------------- EFFET CLIMBING A AJOUTER
+              
+              player.vx = 0
+              
             end
             
             resetcollisions() 
@@ -312,7 +396,11 @@ end
             end
             
             if collisiondroite == false then
-              player.x = player.x + 1  
+              
+            else             ------------------------------- EFFET CLIMBING A AJOUTER
+              
+              player.vx = 0
+              
             end
             
             resetcollisions() 
